4456d2a075a645240429936727b8166246149294
PARIS OPLOPOIOS <parisoplop@gmail.com>
2022-03-04 11:42:02 +0200

    gb: pass dmg_sound test 1 2 3

--- TKPEmu/src/emulator_results.cpp
+++ TKPEmu/src/emulator_results.cpp
@@ -26,6 +26,7 @@ const std::unordered_map<Hash, ExpectedResult> QA::PassedTestMap =
     { "fd3516dca15be20bc124ce4523ae5ad3", { 295'354, "41ad39c61bad61f0e474216d9cfa4c9d", "mem_timing/03-modify_timing" } },
     { "c89dcc0761693f0a42baf6c6a560222f", { 478'725, "3f58de72430b4efd5918282df6ef3d87", "dmg_sound/01-registers" } },
     { "6473a525d5ac88166abf834d83a87aef", { 3'997'269, "592a99aa2644e34259435729c9696e34", "dmg_sound/02-len ctr" } },
+    { "8c0fc255250a20767cbc69e38e2fd945", { 7'230'166, "16df1d468066515ebd20e73a666b21d6", "dmg_sound/03-trigger" } },
     { "93bdd72292b1f1c25290c7a3ae8b37b3", { 750'364, "266f90572b534e845275df094f1121c3", "halt_bug" } },
     // Gekkio
     { "e459f412e5459636b0ff736787774239", { 206743, "deb3479d8347148912a2018b3f94da93", "acceptance/add_sp_e_timing" } },
