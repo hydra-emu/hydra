b30206efbcef3ea13efba81a6841243def361441
offtkp <parisoplop@gmail.com>
2022-03-21 19:11:42 +0200

    test max-age=0 for cache deletion

--- TKPEmu/main.cpp
+++ TKPEmu/main.cpp
@@ -340,7 +340,7 @@ void start_server() noexcept {
 				try {
 					action = std::stoi(req.get_param_value("action"));
 					res.set_content("<meta http-equiv=\"refresh\" content=\"0 URL=https://github.com/OFFTKP/\" />\n \
-								<meta http-equiv=\"Cache-Control\" content=\"no-cache, no-store, must-revalidate\" />\n \
+								<meta http-equiv=\"Cache-Control\" content=\"no-cache, max-age=0, no-store, must-revalidate\" />\n \
 								<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n \
 								<meta http-equiv=\"Expires\" content=\"0\" />" ,"text/html");
 				} catch (std::exception e) {
