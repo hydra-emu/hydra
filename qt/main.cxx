c51b232a76b7f2dddff7f56cd3706860ae8df5ed
offtkp <parisoplop@gmail.com>
2023-07-15 15:21:08 +0300

    Open file from CLI

--- qt/main.cxx
+++ qt/main.cxx
@@ -15,5 +15,9 @@ int main(int argc, char* argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    if (argc > 1)
+    {
+        w.OpenFile(argv[1]);
+    }
     return a.exec();
 }
