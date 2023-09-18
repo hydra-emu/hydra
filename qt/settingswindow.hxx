06e8e9df40c9b7e16544197c36829332b33f03ea
offtkp <parisoplop@gmail.com>
2023-09-18 15:39:01 +0300

    Clean up settings

--- qt/settingswindow.hxx
+++ qt/settingswindow.hxx
@@ -5,6 +5,8 @@
 #include <QListWidget>
 #include <QWidget>
 
+class QGridLayout;
+
 class SettingsWindow : public QWidget
 {
     Q_OBJECT
@@ -17,12 +19,11 @@ private:
     QLineEdit *dmg_bios_path_, *cgb_bios_path_;
     QLineEdit* ipl_path_;
     void create_tabs();
+    void on_open_file_click(const std::string& setting, const std::string& extension);
+    void add_filepicker(QGridLayout* layout, const std::string& name, const std::string& setting,
+                        const std::string& extension, int row, int column);
 private slots:
     void on_tab_change();
-    void on_dmg_click();
-    void on_cgb_click();
-    void on_ipl_click();
-    void on_gb_skip_bios_click(int state);
 
 public:
     SettingsWindow(bool& open, QWidget* parent = nullptr);
