88d2975f23049589202907f4c76a8227f7e23e26
offtkp <parisoplop@gmail.com>
2023-09-27 18:41:03 +0300

    Install script mkdirs

--- scripts/install_hooks_linux.sh
+++ scripts/install_hooks_linux.sh
@@ -5,6 +5,8 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
+mkdir -p .git/hooks
+mkdir -p .git/hooks/pre-commit
 mv .git/hooks/pre-commit .git/hooks/pre-commit.bak
 cp scripts/hooks/pre-commit.linux .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
