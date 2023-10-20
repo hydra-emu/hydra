d95a0379a5899d99c68053336f443431e29ec007
Paris Oplopoios <parisoplop@gmail.com>
2023-10-21 00:03:48 +0200

    This file is important I think

---
+++ ios/hydra_ios/CoreDriver.h
@@ -0,0 +1,12 @@
+//
+//  CoreDriver.h
+//  ios
+//
+//  Created by m1 on 20/10/2023.
+//
+
+#ifndef CoreDriver_h
+#define CoreDriver_h
+
+
+#endif /* CoreDriver_h */
---
+++ ios/hydra_ios/CoreDriver.m
@@ -0,0 +1,8 @@
+//
+//  CoreDriver.m
+//  hydra_ios
+//
+//  Created by m1 on 20/10/2023.
+//
+
+#import <Foundation/Foundation.h>
