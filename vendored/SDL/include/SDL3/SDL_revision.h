d94bb453e012952e4eab5e3b9c17c584c07098fb
OFFTKP <parisoplop@gmail.com>
2024-01-06 19:04:49 +0000

    Deploying to gh-pages from @ hydra-emu/hydra@29add12a6a0c47b15f1ed493925cf9dd72d07023 🚀

--- vendored/SDL/include/SDL3/SDL_revision.h
+++ vendored/SDL/include/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-231920c (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-29add12 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-231920c"
+#define SDL_REVISION "SDL-29add12"
 #endif
 
 #endif /* SDL_revision_h_ */
--- vendored/openssl/crypto/buildinf.h
+++ vendored/openssl/crypto/buildinf.h
@@ -1,6 +1,6 @@
 /* auto-generated for crypto/cversion.c */
 #define CFLAGS "compiler: Clang  -g -O3"
 #define PLATFORM "platform: Emscripten"
-#define DATE "built on: 2024-01-06 18:51:27 UTC"
+#define DATE "built on: 2024-01-06 19:00:34 UTC"
 static const char *compiler_flags = CFLAGS;
 
